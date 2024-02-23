2ff671e3f8382ddfef7e9fa071977ec0ed93dfa5
amare <amarehagos26273424@gmail.com>
2024-02-23 14:36:51 +0300

    commit

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const ErrorHandler = require("./middleware/error");
 const user = require('./controller/user')
 const admin= require('./controller/admin')
 const shop= require('./controller/shop')
+const product= require('./controller/product')
+const event= require('./controller/event')
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -34,6 +36,8 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 app.use("/api/v2/user", user);
 app.use("/api/v2/admin", admin);
 app.use("/api/v2/shop", shop);
+app.use("/api/v2/product", product);
+app.use("/api/v2/event", event);
 // it's for ErrorHandling
 app.use(ErrorHandler);
 
---
+++ backend/model/event.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your event name!"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter your event description!"],
+  },
+  category: {
+    type: String,
+    required: [true, "Please enter your event category!"],
+  },
+  tags: {
+    type: String,
+  },
+  originalPrice: {
+    type: Number,
+  },
+  discountPrice: {
+    type: Number,
+    required: [true, "Please enter your event price!"],
+  },
+  stock: {
+    type: Number,
+    required: [true, "Please enter your event stock!"],
+  },
+  images: [
+   
+       {
+         type: String
+        }
+  ]
+  ,
+  shopId: {
+    type: String,
+    required: true,
+  },
+  shop: {
+    type: Object,
+    required: true,
+  },
+  sold_out: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("event", eventSchema);
